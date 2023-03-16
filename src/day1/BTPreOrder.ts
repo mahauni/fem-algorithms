6f6d4abe5a93b7b8478646516d93eb599dc0368a
Lucas Raoni <lucasraoniha@gmail.com>
2023-03-16 19:33:54 -0300

    Finished binary tree traversals

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // recurse step
+    
+    // pre
+    path.push(curr.value);
+    
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    
+    // post
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
